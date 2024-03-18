501a0a3623a9ae419f583002580a82e89dfd5e3b
ItSolution21 <161233126+ItSolution21@users.noreply.github.com>
2024-03-18 13:10:58 +0530

    Add files via upload

---
+++ thirteen.js
@@ -0,0 +1,54 @@
+//1. string ko reverse
+//2. number ko reverse
+//3. array ko sort
+//4. fabonacci series
+
+for (let i = 10; i <= 1; i++) {
+  console.log(i);
+}
+
+let j = 10;
+do {
+  console.log(j);
+  j++;
+} while (2 > j);
+
+//object - key-value ka pair
+
+let Student = {
+  name: "rohit",
+  class: "12th",
+  age: 20,
+  rollno: 21,
+  address: "vijay Nagar",
+};
+
+console.log(
+  Student.name +
+    " " +
+    Student.class +
+    " " +
+    Student.age +
+    " " +
+    Student.rollno +
+    " " +
+    Student.address
+);
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// *****
+// ****
+// ***
+// **
+// *
+
+// *****
+//  ****
+//   ***
+//    **
+//     *
